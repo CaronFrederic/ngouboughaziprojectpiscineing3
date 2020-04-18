9aa6547e490e02af1090cc91edf3650a3eb9080b
MjSaid03 <47595266+MjSaid03@users.noreply.github.com>
2020-04-18 12:54:57 +0200

    Add files via upload

---
+++ inscription acheteur.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html> 
+
+<html>
+
+<head>
+
+<title>
+
+ECE Ebay
+
+</title>
+
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script> 
+	
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	
+<link rel="stylesheet" type="text/css" href="fot2.css">
+	
+</head> 
+	
+	<body>
+		
+		
+		
+		<br/><br/>
+		<div class="container-fluid">
+		
+		<div class="row">
+			
+			<div class="col-sm-4"><img src="image/logo.png" width="400" height="175" alt="Bootstrap" class="img-responsive"></div>
+			<div class="col-sm-8">
+			
+				<div class="container-fluid">
+				
+			<div class="row">
+				
+				<div class="col-sm-4">  <h1 class="titre" >Cr&eacute;er un Compte !</h1> </div>
+				
+			</div>
+			
+				</div>
+			</div>
+			
+		</div>
+		
+		<div class="form-style">
+			<form action="/my-handling-form-page" method="post"> 
+				<div>
+					<label for="prenom">Prenom :</label>
+					<input type="text" id="prenom" name="name">
+				</div>
+				<div>
+					<label for="nom">Nom :</label>
+					<input type="text" id="nom" name="family_name">
+				</div>
+				<div>
+					<label for="Pseudo">Pseudo (identifant) :</label>
+					<input type="text" id="Pseudo" name="identifiant">
+				</div>
+				<div>
+					<label for="mail">e-mail (mot de passe):</label>
+					<input type="email" id="mail" name="user_mail">
+				</div>
+			</form>
+			<div class="button">
+				<button type="submit">S'inscrire!</button>
+			</div>
+			<p> Vous voulez vendre? Cliquer <a href="#">ici</a> pour cre&eacute; un compte vendeur</p>
+		</div>
+		
+		</div>
+		
+		<br/><br/>	
+		
+		
+		
+		
+		<footer class="page-footer"> 
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-md-8 col-sm-12">
+						<h6 class="text-uppercase font-weight-bold">Information additionnelle</h6> 
+						<p>
+							ECE eBay est une entreprise fran&ccedil;aise de courtage en ligne, connue par son site web de ventes aux ench&egrave;res du m&ecirc;me nom. Elle a &eacute;t&eacute; cr&eacute;&eacute;e en 1995 par le Fran&ccedil;ais Pierre Omidyar. Elle est devenue une r&eacute;f&eacute;rence mondiale dans son secteur et un ph&eacute;nom&egrave;ne de soci&eacute;t&eacute;.
+
+					</p> 
+					<p>
+						Toute personne inscrite sur le site eBay de son pays peut participer aux ench&egrave;res de tous les pays. Il faut &ecirc;tre majeur pour s&apos;inscrire. Une fois que l&apos;inscription est valid&eacute;e, c&apos;est-à-dire :
+
+pour pouvoir acheter : apr&egrave;s v&eacute;rification de l&apos;adresse &eacute;lectronique indiqu&eacute;e à l&apos;inscription ;
+pour pouvoir vendre : apr&egrave;s v&eacute;rification de l&apos;identit&eacute; du membre par courrier postal (justificatif de domicile) ou identification bancaire (par la carte de cr&eacute;dit) ;
+					</p>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-12">
+						<h6 class="text-uppercase font-weight-bold">Contact</h6> 
+						<p>
+							37, quai de Grenelle, 75015 Paris, France <br> ece.ebay@edu.ece.fr <br>
+							+33 01 02 03 04 05 <br>
+							+33 01 03 02 05 04
+						</p> 
+					</div>
+				</div>
+			</div>
+			<div class="footer-copyright text-center">&copy; 2019 Copyright | Droit d'auteur: ece.ebay@edu.ece.fr</div> </footer>
+		
+	</body>
+
+		</html>
